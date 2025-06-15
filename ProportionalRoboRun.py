e2a4789753d57433f3edb3ddee08cd2d34092afe
Elliot Scher <44726531+ElliotScher@users.noreply.github.com>
2025-06-15 16:28:54 -0400

    Update ProportionalRoboRun.py

--- ProportionalRoboRun.py
+++ ProportionalRoboRun.py
@@ -1,30 +1,41 @@
 from XRPLib.defaults import *
 import time
 
+# kp, this will be tuned
 kp = 0.25
 
-# initial ramp
+# drive forward 20 cm at 50% power
 drivetrain.straight(20, (0.5))
+
+# stop 
 drivetrain.stop()
 
 # wait for gate to close
 while rangefinder.distance() > 15:
     pass
 
+# wait 1/4 seconds to make sure the gate is fully down
 time.sleep(0.25)
 
-# wait for gate to open
+# while the gate is closed:
 while rangefinder.distance() < 15:
+    # calculate how far the robot is from the gate
     error = rangefinder.distance() - 5
+
+    # calculate the effort, which is error times kp
     effort = error * kp
+
+    # send the effort to the motor
     drivetrain.set_effort(effort, effort)
 
+# stop
 drivetrain.stop()
 
-# drive through gate
+# wait for 1 second so the gate is fully out of the way
 time.sleep(1)
-drivetrain.straight(40, (0.5))
 
+# drive forward 40 cm
+drivetrain.straight(40, (0.5))
 
 # stop
 drivetrain.stop()
@@ -36,39 +47,61 @@ drivetrain.turn(90)
 while rangefinder.distance() > 15:
     pass
 
-# wait for gate to open
+# while the gate is closed:
 while rangefinder.distance() < 15:
+    # calculate how far the robot is from the gate
     error = rangefinder.distance() - 5
+
+    # calculate the effort, which is error times kp
     effort = error * kp
+
+    # send the effort to the motor
     drivetrain.set_effort(effort, effort)
+
+# stop
 drivetrain.stop()
 
-# drive through gate
+# wait for 1 second so the gate is fully out of the way
 time.sleep(1)
+
+# drive forward 30 cm
 drivetrain.straight(30, (0.5))
 
 # turn 90 degrees
 drivetrain.turn(90)
 
+# wait 1/4 seconds to make sure the gate is fully down
 time.sleep(0.25)
 
+# drive forward 5 cm
 drivetrain.straight(5, (0.5))
+
+# stop
 drivetrain.stop()
 
 # wait for gate to close
 while rangefinder.distance() > 15:
     pass
 
+# wait 1/4 seconds to make sure the gate is fully down
 time.sleep(0.25)
 
 # wait for gate to open
 while rangefinder.distance() < 15:
+    # calculate how far the robot is from the gate
     error = rangefinder.distance() - 5
+
+    # calculate the effort, which is error times kp
     effort = error * kp
+
+    # send the effort to the motor
     drivetrain.set_effort(effort, effort)
 
+# stop
 drivetrain.stop()
 
-# drive through gate
+# wait for 1 second so the gate is fully out of the way
 time.sleep(1)
+
+# drive forward 40 cm
 drivetrain.straight(40, (0.5))
